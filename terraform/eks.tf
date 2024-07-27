adee475074ad70ba8c771d08f41daf042bec5d61
Chris <chris.hickey@vidence.com>
2024-07-27 16:20:01 -0600

    adding backend to k8s

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,18 +4,8 @@ provider "aws" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
+  cluster_name    = "takehome"
   cluster_version = "1.21"
-  subnets         = ["subnet-123456", "subnet-654321"]
   vpc_id          = "vpc-123abc"
 
-  node_groups = {
-    example = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_type = "m5.large"
-    }
-  }
 }
