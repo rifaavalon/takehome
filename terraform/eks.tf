758c6aadce4e50065951b9549177cf925e630d5c
Chris <chris.hickey@vidence.com>
2024-07-27 16:31:35 -0600

    terraform definitions

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,8 +4,24 @@ module "eks" {
 
   cluster_name    = "my-cluster"
   cluster_version = "1.21" # Or any version you prefer
+  vpc_id          = aws_vpc.my_vpc.id
 
-  vpc_id = aws_vpc.my_vpc.id # Move the "vpc_id" attribute here
-
-    
+  fargate_profiles = {
+    example = {
+      name               = "example"
+      pod_execution_role_arn = aws_iam_role.fargate_pod_execution_role.arn
+      subnet_ids         = aws_vpc.my_vpc.private_subnet_ids
+      selector           = [
+        {
+          namespace = "kube-system"
+          labels    = {
+            "k8s-app" = "kube-dns"
+          }
+        },
+        {
+          namespace = "default"
+        },
+      ]
+    }
+  }
 }
