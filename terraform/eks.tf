a759c4a996cfef59f7233084bfa68218bf28726e
Chris <chris.hickey@vidence.com>
2024-07-27 18:48:37 -0600

    fixing kubernetes

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,22 +12,22 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false # Ensures Terraform does not use the default kubeconfig file
+  load_config_file       = false
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name = module.eks.cluster_id
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 17.0"
 
-  cluster_name    = "takehome"
+  cluster_name    = var.cluster_name
   cluster_version = "1.30" # Or any version you prefer
 
   subnets         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
@@ -43,6 +43,7 @@ module "eks" {
       key_name       = aws_key_pair.my_key.key_name
     }
   }
+
 
   node_groups_defaults = {
     iam_role = aws_iam_role.eks_node_role.arn
