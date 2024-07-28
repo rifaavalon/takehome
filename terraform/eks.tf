7c5c954819d80f463ec391033ad433ddab401db7
Chris <chris.hickey@vidence.com>
2024-07-27 18:58:09 -0600

    taking out load config file argument

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,7 +12,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
 }
 
 data "aws_eks_cluster" "cluster" {
