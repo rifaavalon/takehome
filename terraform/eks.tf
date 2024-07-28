ad05ffdb9be930ec0fe641a4aab87c7d13d163ad
Chris <chris.hickey@vidence.com>
2024-07-27 19:02:31 -0600

    taking out load config file argument

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -18,6 +18,9 @@ data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 17.0"
