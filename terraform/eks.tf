c23ba071acf7cf2a1f5809db29eca1e516fcd605
Chris <chris.hickey@vidence.com>
2024-07-27 21:13:20 -0600

    adding in a cluster

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,7 +11,7 @@ resource "aws_key_pair" "my_key" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = module.eks.aws_eks_cluster_auth.cluster.token
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 data "aws_eks_cluster_auth" "cluster" {
